de96aeaa0c75968b82468293fe7a2ed3dc052302
Julieta <92743415+julietamorrazo@users.noreply.github.com>
2022-08-29 18:04:39 -0300

    Add files via upload

---
+++ AP-PorfolioWeb/portfolio1/enviar.js
@@ -0,0 +1,9 @@
+const botonEnviar = document.getElementById("boton");
+
+function enviarFormularioContactame(){
+	alert("Su mensaje se enviado correctamente");
+}
+
+botonEnviar.addEventListener('click', function(){
+	enviarFormularioContactame();
+});
